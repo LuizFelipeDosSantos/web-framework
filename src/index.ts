c0ac45f621ee42b928649508e35b0555a3503822
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2025-07-30 12:59:40 +0200

    added change name feature

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import { UserForm } from "./views/UserForm";
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
-const userForm = new UserForm(document.getElementById('root') as Element, user);
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+
+  userForm.render();
+} else {
+  throw new Error('Root element not found!');
+
+}
